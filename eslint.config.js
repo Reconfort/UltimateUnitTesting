ef0e08abc436e3aa22befac412403db42b6d3445
Reconfort <reconfortdanny@gmail.com>
2024-04-10 08:48:55 +0200

    adding husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
-import styleGuide from 'eslint-config-standard';
+import styleGuide from "eslint-config-standard";
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+
 
 export default [
   ...[].concat(styleGuide),
@@ -10,5 +11,5 @@ export default [
       'no-undef': 'error',
     },
   },
-
-];
+]
+tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended);
